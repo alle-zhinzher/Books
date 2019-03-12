4edd39ff45da4f34a09bf6f95cb56137e060da68
zhinzher <sanches2000009@gmail.com>
2019-03-12 23:41:37 +0200

    oop

---
+++ Pure_Python/OOP/exception_classes.py
@@ -0,0 +1,15 @@
+class BaseValidationError(ValueError):
+    pass
+
+class NameTooShortError(BaseValidationError):
+    pass
+
+class NameTooLongError(BaseValidationError):
+    pass
+
+class NameTooCuteError(BaseValidationError):
+    pass
+
+def validate(name):
+    if len(name) < 10:
+        raise NameTooShortError(name)
