c44451afebfe69d37e69bf01ce9689a4d89fd66c
zhinzher <sanches2000009@gmail.com>
2019-04-06 11:35:33 +0300

    Added Flywight pattern

--- Patterns/Facade.py
+++ Patterns/Facade.py
@@ -94,10 +94,13 @@ class OperatingSystem:
     def __init__(self):
         self.fs = FileServer()
         self.ps = ProcessServer()
+    
     def start(self):
         [i.boot() for i in (self.fs, self.ps)]
+    
     def create_file(self, user, name, permissions):
         return self.fs.create_file(user, name, permissions)
+    
     def create_process(self, user, name):
         return self.ps.create_process(user, name)
 
